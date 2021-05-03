26feaf1d850bda5e02d71025e36bad50bb3b2346
natalia.palota <natalia.palota@zup.com.br>
2021-05-03 15:39:49 -0300

    Criar entity de Guildas

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/entities/Guilda.java
@@ -0,0 +1,29 @@
+package com.br.zup.gerenciadordeguildas.entities;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "guildas")
+public class Guilda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private String objetivos;
+    private String grupo;
+    @OneToMany()
+    private List<Representante> representantes;
+    @OneToMany
+    private List<Atividade> atividades;
+
+
+
+}
