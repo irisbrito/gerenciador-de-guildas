92f1fb4086e0f44fc62301bb1f8a51c79ceb3c89
natalia.palota <natalia.palota@zup.com.br>
2021-05-03 17:07:59 -0300

    Criar entity Atas

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/entities/Atas.java
@@ -0,0 +1,22 @@
+package com.br.zup.gerenciadordeguildas.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "atas")
+public class Atas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDate data;
+    private String titulo;
+    private String assuntos;
+    private String resultado;
+
+}
