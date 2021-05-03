38897267de1c6bd5e138935e95761d53664cdf90
Iris <iris.souza@zup.com.br>
2021-05-03 14:55:41 -0300

    Criar projeto

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/GerenciadorDeGuildasApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.gerenciadordeguildas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeGuildasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeGuildasApplication.class, args);
+	}
+
+}
