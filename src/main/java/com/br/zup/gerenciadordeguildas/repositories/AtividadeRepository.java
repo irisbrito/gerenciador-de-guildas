23915eb370c0028ce796157bd2074553d397fed5
Iris <iris.souza@zup.com.br>
2021-05-03 17:02:04 -0300

    Adicionar interface AtividadeRepository

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/repositories/AtividadeRepository.java
@@ -0,0 +1,7 @@
+package com.br.zup.gerenciadordeguildas.repositories;
+
+import com.br.zup.gerenciadordeguildas.entities.Atividade;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AtividadeRepository extends CrudRepository<Atividade, Integer> {
+}
