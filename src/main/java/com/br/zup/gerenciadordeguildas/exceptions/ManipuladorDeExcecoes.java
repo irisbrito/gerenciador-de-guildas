8b78b6f6c68159980655232a7b178a07668c88a1
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-05-04 15:02:44 -0300

    Adicionar interceptador listaVaziaException no ManipuladorDeExcecoes no pacote exceptions

--- src/main/java/com/br/zup/gerenciadordeguildas/exceptions/ManipuladorDeExcecoes.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/exceptions/ManipuladorDeExcecoes.java
@@ -1,7 +1,29 @@
 package com.br.zup.gerenciadordeguildas.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @RestControllerAdvice
 public class ManipuladorDeExcecoes {
+
+    @ExceptionHandler(ListaVaziaException.class)
+    public ResponseEntity<MensagemDeErro> listaVaziaException(
+            ListaVaziaException exception,
+            HttpServletRequest request) {
+        String error = "Resource Not Found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        MensagemDeErro err = new MensagemDeErro(
+                Instant.now(),
+                status.value(),
+                error,
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(err);
+    }
 }
