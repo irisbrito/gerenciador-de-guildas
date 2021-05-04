e875476880f05540517aaa6bc518beda426cfc8a
Iris <iris.souza@zup.com.br>
2021-05-04 15:37:32 -0300

    Refatorar recursoNaoEncontradoException

--- src/main/java/com/br/zup/gerenciadordeguildas/exceptions/AtividadeNaoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package com.br.zup.gerenciadordeguildas.exceptions;
-
-public class AtividadeNaoEncontradaException extends RuntimeException {
-
-    public AtividadeNaoEncontradaException(String message) {
-        super(message);
-    }
-}
