3eadff872a4d3d5e420d4d9394eb4645c3f8e1c1
Iris <iris.souza@zup.com.br>
2021-05-04 14:43:01 -0300

    Adicionar exceção de atividade não encontrada

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/exceptions/AtividadeNaoEncontradaException.java
@@ -0,0 +1,8 @@
+package com.br.zup.gerenciadordeguildas.exceptions;
+
+public class AtividadeNaoEncontradaException extends RuntimeException {
+
+    public AtividadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
