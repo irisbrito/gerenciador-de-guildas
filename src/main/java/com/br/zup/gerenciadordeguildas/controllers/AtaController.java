b5d02edf1dcf8c428aacd86b569472d874fc6a9b
natalia.palota <natalia.palota@zup.com.br>
2021-05-04 16:42:59 -0300

    Criar método para adicionarAta no Controller e inserir modelMapper

--- src/main/java/com/br/zup/gerenciadordeguildas/controllers/AtaController.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/controllers/AtaController.java
@@ -2,20 +2,35 @@ package com.br.zup.gerenciadordeguildas.controllers;
 
 import com.br.zup.gerenciadordeguildas.entities.Ata;
 import com.br.zup.gerenciadordeguildas.services.AtaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("atas/")
 public class AtaController {
 
     private AtaService ataService;
+    private ModelMapper modelMapper;
+
+    public AtaController(AtaService ataService, ModelMapper modelMapper) {
+        this.ataService = ataService;
+        this.modelMapper = modelMapper;
+    }
 
     public AtaController(AtaService ataService) {
         this.ataService = ataService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AtaDTO adicionarAta(@RequestBody @Valid AtaDTO ataDTO){
+        Ata ata = modelMapper.map(ataDTO, Ata.class);
+        ata = ataService.cadastrarAtas(ata);
+
+        return modelMapper.map(ata, AtaDTO.class);
     }
 
     @GetMapping("/")
@@ -23,4 +38,6 @@ public class AtaController {
     public Iterable<Ata> buscarAtas() {
         return ataService.buscarAtas();
     }
+
+
 }
