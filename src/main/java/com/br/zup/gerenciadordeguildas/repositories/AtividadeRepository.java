27f70e16a230440808e6181a540efeb03b47adc1
Íris Brito <65046215+irisbrito@users.noreply.github.com>
2021-05-04 13:10:46 -0300

    Revert "Feature/criar atividade dto"

--- src/main/java/com/br/zup/gerenciadordeguildas/repositories/AtividadeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.zup.gerenciadordeguildas.repositories;
-
-import com.br.zup.gerenciadordeguildas.entities.Atividade;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AtividadeRepository extends CrudRepository<Atividade, Integer> {
-}
