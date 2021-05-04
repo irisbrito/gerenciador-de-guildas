f11e0028ac4f42319787079a38217d1c00405de5
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-05-04 14:49:05 -0300

    Adicionar endpoint buscarAtas na classe AtaController no pacote controllers

--- src/main/java/com/br/zup/gerenciadordeguildas/controllers/AtaController.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/controllers/AtaController.java
@@ -1,7 +1,11 @@
 package com.br.zup.gerenciadordeguildas.controllers;
 
+import com.br.zup.gerenciadordeguildas.entities.Ata;
 import com.br.zup.gerenciadordeguildas.services.AtaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +18,9 @@ public class AtaController {
         this.ataService = ataService;
     }
 
-
-    
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public Iterable<Ata> buscarAtas() {
+        return ataService.buscarAtas();
+    }
 }
