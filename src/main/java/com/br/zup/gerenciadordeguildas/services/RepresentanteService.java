9a5c3c0e984300707f0129fdcbc7a2bf63809b6a
Iris <iris.souza@zup.com.br>
2021-05-04 16:14:52 -0300

    Limpar código - remover imports desnecessários

--- src/main/java/com/br/zup/gerenciadordeguildas/services/RepresentanteService.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/services/RepresentanteService.java
@@ -6,8 +6,6 @@ import com.br.zup.gerenciadordeguildas.repositories.RepresentanteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class RepresentanteService {
 
