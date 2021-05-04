d69728ed7566d850559395942b9985586e7a7c79
natalia.palota <natalia.palota@zup.com.br>
2021-05-04 15:52:19 -0300

    Criar GuildaService e criar métodos

--- src/main/java/com/br/zup/gerenciadordeguildas/services/GuildaService.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/services/GuildaService.java
@@ -1,2 +1,19 @@
-package com.br.zup.gerenciadordeguildas.services;public class GuildaService {
+package com.br.zup.gerenciadordeguildas.services;
+
+import com.br.zup.gerenciadordeguildas.entities.Guilda;
+import com.br.zup.gerenciadordeguildas.entities.Representante;
+import com.br.zup.gerenciadordeguildas.repositories.GuildaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GuildaService {
+
+    @Autowired
+    private GuildaRepository guildaRepository;
+
+    public Guilda cadastrarGuilda(Guilda guilda) {
+        return guildaRepository.save(guilda);
+    }
+
 }
