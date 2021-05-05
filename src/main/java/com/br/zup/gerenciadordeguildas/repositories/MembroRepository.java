0a83e3bab649edaad8405b19628774cc55c78e29
Iris <iris.souza@zup.com.br>
2021-05-04 21:22:38 -0300

    Renomear repositorio membroRepository

--- src/main/java/com/br/zup/gerenciadordeguildas/repositories/MembroRepository.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/repositories/MembroRepository.java
@@ -3,6 +3,6 @@ package com.br.zup.gerenciadordeguildas.repositories;
 import com.br.zup.gerenciadordeguildas.entities.Membro;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RepresentanteRepository extends CrudRepository<Membro,Integer> {
+public interface MembroRepository extends CrudRepository<Membro,Integer> {
     Iterable<Membro> findAllByGuildasNome(String nome);
 }
