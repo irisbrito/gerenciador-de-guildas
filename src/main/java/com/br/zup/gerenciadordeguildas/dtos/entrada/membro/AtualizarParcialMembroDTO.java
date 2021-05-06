71cd396c7d3954941ea2fc7d486efcb657a05b12
natalia.palota <natalia.palota@zup.com.br>
2021-05-06 16:42:01 -0300

    Criar DTO para atualizar parcial membros

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/dtos/entrada/membro/AtualizarParcialMembroDTO.java
@@ -0,0 +1,35 @@
+package com.br.zup.gerenciadordeguildas.dtos.entrada.membro;
+
+import com.br.zup.gerenciadordeguildas.entities.Guilda;
+import com.br.zup.gerenciadordeguildas.entities.Membro;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AtualizarParcialMembroDTO {
+
+    private String nome;
+    @Email(message = "Email inválido")
+    private String email;
+
+    private String zenity;
+
+    private List<Guilda> guildas;
+
+    public Membro converterDTOParaModel(Integer id){
+        Membro membro = new Membro();
+        membro.setId(id);
+        membro.setNome(this.nome);
+        membro.setEmail(this.email);
+        membro.setZenity(this.zenity);
+        membro.setGuildas(this.guildas);
+
+        return membro;
+    }
+}
