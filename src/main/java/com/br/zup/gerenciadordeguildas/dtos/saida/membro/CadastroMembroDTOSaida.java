5f153a739df0db190a579bd9ab9a82f07f6a2f3c
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-05-07 02:00:06 -0300

    Adicionar CadastroMembrosDTOSaida no pacote dtos/saida/membro

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/dtos/saida/membro/CadastroMembroDTOSaida.java
@@ -0,0 +1,33 @@
+package com.br.zup.gerenciadordeguildas.dtos.saida.membro;
+
+import com.br.zup.gerenciadordeguildas.entities.Guilda;
+import com.br.zup.gerenciadordeguildas.entities.Membro;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CadastroMembroDTOSaida {
+        private Integer id;
+        private String nome;
+        private String email;
+        private String zenity;
+        private boolean representante;
+        private List<Guilda> guildas;
+
+        public static CadastroMembroDTOSaida converterEntityParaDTOSaida(Membro membro) {
+            CadastroMembroDTOSaida cadastroMembroDTOSaida = new CadastroMembroDTOSaida();
+            cadastroMembroDTOSaida.setId(membro.getId());
+            cadastroMembroDTOSaida.setNome(membro.getNome());
+            cadastroMembroDTOSaida.setEmail(membro.getEmail());
+            cadastroMembroDTOSaida.setZenity(membro.getZenity());
+            cadastroMembroDTOSaida.setRepresentante(membro.isRepresentante());
+            cadastroMembroDTOSaida.setGuildas(membro.getGuildas());
+
+            return cadastroMembroDTOSaida;
+        }
+}
