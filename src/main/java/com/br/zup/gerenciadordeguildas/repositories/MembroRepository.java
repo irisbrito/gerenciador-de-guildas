69114a6cd8f8459e94302ce82c795ea0bf3b00a0
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-05-07 23:01:54 -0300

    Adicionar metodo findByNome na interface MembroRepository

--- src/main/java/com/br/zup/gerenciadordeguildas/repositories/MembroRepository.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/repositories/MembroRepository.java
@@ -3,5 +3,8 @@ package com.br.zup.gerenciadordeguildas.repositories;
 import com.br.zup.gerenciadordeguildas.entities.Membro;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MembroRepository extends CrudRepository<Membro,Integer> {
+    Optional<Membro> findByNome(String nome);
 }
