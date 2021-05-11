a7962158782276838b0018894540576977230c57
Iris <iris.souza@zup.com.br>
2021-05-11 17:18:21 -0300

    Criar classe de exceção e-mail existente

--- src/main/java/com/br/zup/gerenciadordeguildas/exceptions/EmailExistenteException.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/exceptions/EmailExistenteException.java
@@ -1,6 +1,6 @@
 package com.br.zup.gerenciadordeguildas.exceptions;
 
-public class EmailExistenteException extends RuntimeException{
+public class EmailExistenteException extends RuntimeException {
     public EmailExistenteException() {
         super("Membro já cadastrado com este e-mail!");
     }
