436209d5489516248cf859704b1c6585dced8e4d
natalia.palota <natalia.palota@zup.com.br>
2021-05-11 20:12:54 -0300

    Criar AtualizarAtaParcialDTOSaida

--- src/main/java/com/br/zup/gerenciadordeguildas/dtos/entrada/ata/AtualizarAtaParcialDTO.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/dtos/entrada/ata/AtualizarAtaParcialDTO.java
@@ -4,7 +4,7 @@ import com.br.zup.gerenciadordeguildas.entities.Ata;
 import com.br.zup.gerenciadordeguildas.entities.Guilda;
 import lombok.*;
 
-import java.time.LocalDate;
+
 
 @Getter
 @Setter
@@ -13,15 +13,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AtualizarParcialAtaDTO {
 
-    private LocalDate data;
     private String pauta;
     private String assuntos;
     private String guilda;
 
     public Ata converterDTOParaModel(Integer id, Guilda guilda){
-       Ata ata = new Ata();
+        Ata ata = new Ata();
         ata.setId(id);
-        ata.setData(this.data);
         ata.setAssuntos(this.assuntos);
         ata.setGuilda(guilda);
 
---
+++ src/main/java/com/br/zup/gerenciadordeguildas/dtos/saida/ata/AtualizarAtaParcialDTOSaida.java
@@ -0,0 +1,21 @@
+package com.br.zup.gerenciadordeguildas.dtos.saida.ata;
+
+import com.br.zup.gerenciadordeguildas.entities.Ata;
+
+public class AtualizarAtaParcialDTOSaida {
+
+    private String pauta;
+    private String assuntos;
+    private String guilda;
+
+    //public static AtualizarAtaParcialDTOSaida atualizarAtaParcialDTOSaida(Ata ata) {
+        //AtualizarAtaParcialDTOSaida atualizarAtaParcialDTOSaida = new AtualizarAtaParcialDTOSaida();
+        //atualizarAtaParcialDTOSaida.setId(ata.getId());
+        //cadastroAtaDTOSaida.setPauta(ata.getPauta());
+       // cadastroAtaDTOSaida.setData(ata.getData());
+        //cadastroAtaDTOSaida.setAssuntos(ata.getAssuntos());
+        //cadastroAtaDTOSaida.setGuilda(ata.getGuilda());
+
+        //return cadastroAtaDTOSaida;
+    //}
+}
