08a0f1c8b8bed62e226b927c94f903017f144ae5
natalia.palota <natalia.palota@zup.com.br>
2021-05-11 14:50:54 -0300

    Modificar nome da Exception

--- src/main/java/com/br/zup/gerenciadordeguildas/exceptions/NegocioException.java
+++
@@ -1,7 +0,0 @@
-package com.br.zup.gerenciadordeguildas.exceptions;
-
-public class NegocioException extends RuntimeException{
-    public NegocioException(String message) {
-        super(message);
-    }
-}
