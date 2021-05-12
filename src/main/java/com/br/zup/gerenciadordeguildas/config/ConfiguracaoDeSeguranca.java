bcc6775bf65c57dcca6486564a33c68193cbfcf3
Iris <iris.souza@zup.com.br>
2021-05-12 19:08:00 -0300

    Adicionar configuração do cors

--- src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
@@ -1,10 +1,14 @@
 package com.br.zup.gerenciadordeguildas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,4 +25,12 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated();
     }
+
+    @Bean
+    CorsConfigurationSource configuracaoDeCors(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
 }
