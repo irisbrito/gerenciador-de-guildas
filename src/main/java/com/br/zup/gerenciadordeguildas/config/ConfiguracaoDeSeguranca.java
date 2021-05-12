4436925b33f7e362a73e5b4f3cb758831b032008
Iris <iris.souza@zup.com.br>
2021-05-12 20:25:11 -0300

    Adicionar método sobreescrito configure(AutenticationManagerBuilder) na classe de Configuração de Segurança

--- src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,9 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
     @Autowired
     private ComponenteJWT componenteJWT;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -34,6 +38,11 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         http.addFilter(new FiltroDeAutenticacao(componenteJWT, authenticationManager()));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
