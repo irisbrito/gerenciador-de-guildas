10157cdc23b3105b44ddc1c445be12cb1999a838
natalia.palota <natalia.palota@zup.com.br>
2021-05-11 23:34:00 -0300

    Atualizar método Patch na controller

--- src/main/java/com/br/zup/gerenciadordeguildas/controllers/AtaController.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/controllers/AtaController.java
@@ -3,11 +3,13 @@ package com.br.zup.gerenciadordeguildas.controllers;
 import com.br.zup.gerenciadordeguildas.dtos.entrada.ata.AtualizarAtaDTO;
 import com.br.zup.gerenciadordeguildas.dtos.entrada.ata.AtualizarAtaParcialDTO;
 import com.br.zup.gerenciadordeguildas.dtos.entrada.ata.CadastroAtaDTO;
+import com.br.zup.gerenciadordeguildas.dtos.saida.ata.AtualizarAtaParcialDTOSaida;
 import com.br.zup.gerenciadordeguildas.dtos.saida.ata.CadastroAtaDTOSaida;
 import com.br.zup.gerenciadordeguildas.entities.Ata;
 import com.br.zup.gerenciadordeguildas.entities.Guilda;
 import com.br.zup.gerenciadordeguildas.services.AtaService;
 import com.br.zup.gerenciadordeguildas.services.GuildaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,12 +21,12 @@ public class AtaController {
 
     private AtaService ataService;
     private GuildaService guildaService;
+    private ModelMapper modelMapper;
 
-
-
-    public AtaController(AtaService ataService, GuildaService guildaService) {
+    public AtaController(AtaService ataService, GuildaService guildaService, ModelMapper modelMapper) {
         this.ataService = ataService;
         this.guildaService = guildaService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
@@ -43,32 +45,33 @@ public class AtaController {
     }
 
     @GetMapping("guilda/{nome}/")
-    public Iterable<Ata> buscarAtasPeloNomeDaGuilda(@PathVariable String nome){
+    public Iterable<Ata> buscarAtasPeloNomeDaGuilda(@PathVariable String nome) {
         return ataService.buscarAtasPeloNomeDaGuilda(nome);
     }
 
     @GetMapping("{id}/")
-    public Ata buscarAtaPeloId(@PathVariable Integer id){
+    public Ata buscarAtaPeloId(@PathVariable Integer id) {
         return ataService.buscarAtaPeloId(id);
     }
 
     @PutMapping("{id}/")
     @ResponseStatus(HttpStatus.OK)
-    public Ata atualizarAta(@PathVariable Integer id, @RequestBody AtualizarAtaDTO ataDTO){
+    public Ata atualizarAta(@PathVariable Integer id, @RequestBody AtualizarAtaDTO ataDTO) {
         Ata ata = ataService.atualizarAta(ataDTO.converterDTOParaEntity(id));
 
         return ata;
     }
 
     @PatchMapping("{id}/")
-    public Ata atualizarAtaParcial(@PathVariable int id,
-                                   @RequestBody @Valid AtualizarAtaParcialDTO ataDTO){
+    @ResponseStatus(HttpStatus.OK)
+    public AtualizarAtaParcialDTOSaida atualizarAtaParcial(@PathVariable int id,
+                                                           @RequestBody @Valid AtualizarAtaParcialDTO ataDTO){
         Ata ata = ataDTO.converterDTOParaEntity(id);
         if (ataDTO.getGuilda() != null) {
             ata.setGuilda(guildaService.buscarGuildaPeloNome(ataDTO.getGuilda()));
         }
         ata = ataService.atualizarAtaParcial(ata);
-        return ata;
+        return modelMapper.map(ata, AtualizarAtaParcialDTOSaida.class);
     }
 
     @DeleteMapping("{id}/")
