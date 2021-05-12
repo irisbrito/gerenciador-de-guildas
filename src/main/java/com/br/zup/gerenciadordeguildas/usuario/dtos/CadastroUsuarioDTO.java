37e12533226aceb7178c225bd426a5cfd4bea9b4
Iris <iris.souza@zup.com.br>
2021-05-12 19:37:23 -0300

    Criar dto CadastroUsuarioDTO

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/usuario/dtos/CadastroUsuarioDTO.java
@@ -0,0 +1,29 @@
+package com.br.zup.gerenciadordeguildas.usuario.dtos;
+
+import com.br.zup.gerenciadordeguildas.usuario.Usuario;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+public class CadastroUsuarioDTO {
+
+    @Size(min = 10, max = 100, message = "")
+    private String nomeCompleto;
+    @Email(message = "E-mail inválido!")
+    private String email;
+    @Size(min = 6, max = 10, message = "Senha inválida, deve conter no mín 6 caracteres e no máx 10")
+    private String senha;
+
+    public Usuario converterDTOParaModel(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail(this.email);
+        usuario.setNomeCompleto(this.nomeCompleto);
+        usuario.setSenha(this.senha);
+
+        return usuario;
+    }
+}
