9c34378bd386e99c18fc8d5c81b7209554371e59
Iris <iris.souza@zup.com.br>
2021-05-12 20:36:16 -0300

    Adicionar método para permissão ao usuário (getAutenticacao) na classe de Filtro de Autorização

--- src/main/java/com/br/zup/gerenciadordeguildas/jwt/FiltroDeAutorizacao.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/jwt/FiltroDeAutorizacao.java
@@ -1,8 +1,14 @@
 package com.br.zup.gerenciadordeguildas.jwt;
 
+import com.br.zup.gerenciadordeguildas.exceptions.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class FiltroDeAutorizacao extends BasicAuthenticationFilter {
 
@@ -14,4 +20,18 @@ public class FiltroDeAutorizacao extends BasicAuthenticationFilter {
         this.componenteJWT = componenteJWT;
         this.userDetailsService = userDetailsService;
     }
+
+    private UsernamePasswordAuthenticationToken getAutenticacao(HttpServletRequest request, String token){
+
+        if(!componenteJWT.isTokenValid(token)){
+            throw new TokenInvalidoException();
+        }
+
+        Claims claims = componenteJWT.getClaims(token);
+        UserDetails user = userDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+    }
+
 }
