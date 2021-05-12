12f01fd9c6235e59e7bc50f86815bbac29acf09e
Iris <iris.souza@zup.com.br>
2021-05-12 19:53:06 -0300

    Adicionar query findByEmail em UsuarioRepository

--- src/main/java/com/br/zup/gerenciadordeguildas/usuario/UsuarioRepository.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/usuario/UsuarioRepository.java
@@ -2,5 +2,9 @@ package com.br.zup.gerenciadordeguildas.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+    Optional<Usuario> findByEmail(String email);
+
 }
