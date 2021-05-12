4c74325a214181ff780e2718458e385f933f4d57
Iris <iris.souza@zup.com.br>
2021-05-12 19:19:08 -0300

    Adicionar UsuarioRepository

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.br.zup.gerenciadordeguildas.usuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+}
