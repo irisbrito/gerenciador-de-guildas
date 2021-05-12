025944668dbdc4b38c5217fc8f208681e481d08e
Iris <iris.souza@zup.com.br>
2021-05-12 19:20:52 -0300

    Adicionar método de salvarUsuario em UsuarioService

---
+++ src/main/java/com/br/zup/gerenciadordeguildas/usuario/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.br.zup.gerenciadordeguildas.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvarUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+}
