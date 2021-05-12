3a6522c5d5531ca8c2274327bde79d36f455b217
Iris <iris.souza@zup.com.br>
2021-05-12 19:11:35 -0300

    Adicionar método do bCryptPasswordEncoder

--- src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,4 +34,8 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
         return source;
     }
 
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
