574181d9eb077d106508edca86d42e8952ddb551
Iris <iris.souza@zup.com.br>
2021-05-12 14:25:49 -0300

    Alterar método de conversão de dto para entity para atualizar uma Ata (PUT)

--- src/main/java/com/br/zup/gerenciadordeguildas/dtos/entrada/ata/AtualizarAtaDTO.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/dtos/entrada/ata/AtualizarAtaDTO.java
@@ -1,18 +1,26 @@
 package com.br.zup.gerenciadordeguildas.dtos.entrada.ata;
 
 import com.br.zup.gerenciadordeguildas.entities.Ata;
+import com.br.zup.gerenciadordeguildas.entities.Guilda;
+import lombok.*;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtualizarAtaDTO {
 
     private String pauta;
     private String assuntos;
     private String guilda;
 
-    public Ata converterDTOParaEntity(Integer id) {
+    public Ata converterDTOParaEntity(Integer id, Guilda guilda) {
         Ata ata = new Ata();
         ata.setId(id);
         ata.setPauta(this.pauta);
         ata.setAssuntos(this.assuntos);
+        ata.setGuilda(guilda);
 
         return ata;
     }
