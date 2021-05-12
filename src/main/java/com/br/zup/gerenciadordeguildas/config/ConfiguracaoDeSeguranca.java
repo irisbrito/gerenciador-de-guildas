8cb73d1d5c4479c8adc2a6bdda18b3d2653793f8
Iris <iris.souza@zup.com.br>
2021-05-12 19:06:54 -0300

    Incluir autorização de requisições em determinados endpoints no método configure

--- src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
@@ -1,6 +1,7 @@
 package com.br.zup.gerenciadordeguildas.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,5 +15,10 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.cors();
 
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/atas/**").permitAll()
+                .antMatchers("/atividades/**").permitAll()
+                .antMatchers("/guildas/").permitAll()
+                .anyRequest()
+                .authenticated();
     }
 }
