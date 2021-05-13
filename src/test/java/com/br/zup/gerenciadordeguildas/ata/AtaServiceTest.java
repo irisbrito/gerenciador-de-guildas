bc01db621c3a1e9b720ad83e153fdc39d16be0e1
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-05-13 15:43:34 -0300

    Metodo setUp configurado

--- src/test/java/com/br/zup/gerenciadordeguildas/ata/AtaServiceTest.java
+++ src/test/java/com/br/zup/gerenciadordeguildas/ata/AtaServiceTest.java
@@ -1,22 +1,30 @@
 package com.br.zup.gerenciadordeguildas.ata;
 
 import com.br.zup.gerenciadordeguildas.entities.Ata;
+import com.br.zup.gerenciadordeguildas.entities.Atividade;
 import com.br.zup.gerenciadordeguildas.entities.Guilda;
 import com.br.zup.gerenciadordeguildas.entities.Membro;
+import com.br.zup.gerenciadordeguildas.enums.Status;
 import com.br.zup.gerenciadordeguildas.repositories.AtaRepository;
 import com.br.zup.gerenciadordeguildas.services.AtaService;
+import com.br.zup.gerenciadordeguildas.services.AtividadeService;
+import com.br.zup.gerenciadordeguildas.services.GuildaService;
+import com.br.zup.gerenciadordeguildas.services.MembroService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.time.Instant;
+import java.util.Arrays;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@ContextConfiguration(classes = AtaService.class)
 public class AtaServiceTest {
 
     @Autowired
@@ -25,36 +33,56 @@ public class AtaServiceTest {
     @MockBean
     AtaRepository ataRepository;
 
+    @MockBean
+    GuildaService guildaService;
+
+    @MockBean
+    MembroService membroService;
+
+    @MockBean
+    AtividadeService atividadeService;
+
     private Ata ata;
     private Guilda guilda;
     private Membro membro;
+    private Atividade atividade;
 
     @BeforeEach
     public void setUp() {
-        this.membro = new Membro();
-        membro.setId(1);
-        membro.setNome("NomeMembro para teste.");
-        membro.setEmail("emailmembro@teste.com");
-        membro.setZenity("Zenity para teste.");
-        membro.setRepresentante(false);
-        //membro.setGuilda(); verificar com o mentor
-
-        this.guilda = new Guilda();
-        guilda.setId(1);
-        guilda.setNome("NomeGuilda para teste.");
-        guilda.setDescricao("Descrição para teste.");
-        guilda.setObjetivos("Objetivos para teste.");
-        guilda.setLinkDoChat("LinkDoChat para teste.");
-        //guilda.setMembros(); verificar com o mentor
-        //guilda.setAtividades(); verificar com o mentor
-        //guilda.setAtas(); verificar com o mentor
 
         this.ata = new Ata();
-        ata.setId(1);
-        ata.setData(Instant.now());
-        ata.setPauta("Pauta para teste.");
-        ata.setAssuntos("Assuntos para teste.");
-        ata.setGuilda(guilda);
+        this.ata.setId(1);
+        this.ata.setData(Instant.now());
+        this.ata.setPauta("Pauta para teste.");
+        this.ata.setAssuntos("Assuntos para teste.");
+
+        this.membro = new Membro();
+        this.membro.setId(1);
+        this.membro.setNome("NomeMembro para teste.");
+        this.membro.setEmail("emailmembro@teste.com");
+        this.membro.setZenity("Zenity para teste.");
+        this.membro.setRepresentante(false);
+
+        this.atividade = new Atividade();
+        this.atividade.setId(1);
+        this.atividade.setNome("NomeAtividade para teste");
+        this.atividade.setDescricao("DescriçãoAtividade para teste");
+        this.atividade.setStatus(Status.PENDENTE);
+        this.atividade.setResponsaveis(Arrays.asList(membro));
+
+        this.guilda = new Guilda();
+        this.guilda.setId(1);
+        this.guilda.setNome("NomeGuilda para teste.");
+        this.guilda.setDescricao("Descrição para teste.");
+        this.guilda.setObjetivos("Objetivos para teste.");
+        this.guilda.setLinkDoChat("LinkDoChat para teste.");
+        this.guilda.setMembros(Arrays.asList(membro));
+        guilda.setAtividades(Arrays.asList(atividade));
+        this.guilda.setAtas(Arrays.asList(ata));
+
+        this.membro.setGuilda(guilda);
+        this.ata.setGuilda(guilda);
+        this.atividade.setGuilda(guilda);
     }
 
     @Test
