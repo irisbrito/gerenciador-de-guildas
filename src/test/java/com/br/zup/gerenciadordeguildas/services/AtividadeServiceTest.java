bbcb50ea218366d23c9a134d5e1d6f36cd8d989f
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-05-14 00:11:20 -0300

    Adicionar metodo setUp configurado

--- src/test/java/com/br/zup/gerenciadordeguildas/services/AtividadeServiceTest.java
+++ src/test/java/com/br/zup/gerenciadordeguildas/services/AtividadeServiceTest.java
@@ -3,11 +3,15 @@ package com.br.zup.gerenciadordeguildas.services;
 import com.br.zup.gerenciadordeguildas.entities.Atividade;
 import com.br.zup.gerenciadordeguildas.entities.Guilda;
 import com.br.zup.gerenciadordeguildas.entities.Membro;
+import com.br.zup.gerenciadordeguildas.enums.Status;
 import com.br.zup.gerenciadordeguildas.repositories.AtaRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Arrays;
 
 @SpringBootTest
 @ContextConfiguration(classes = AtividadeService.class)
@@ -21,6 +25,36 @@ public class AtividadeServiceTest {
 
 
     private Atividade atividade;
-    private Guilda guilda;
     private Membro membro;
+    private Guilda guilda;
+
+    @BeforeEach
+    public void setUp() {
+
+        this.membro = new Membro();
+        this.membro.setId(1);
+        this.membro.setNome("NomeMembro para teste.");
+        this.membro.setEmail("emailmembro@teste.com");
+        this.membro.setZenity("Zenity para teste.");
+        this.membro.setRepresentante(false);
+
+        this.atividade = new Atividade();
+        this.atividade.setId(1);
+        this.atividade.setNome("NomeAtividade para teste");
+        this.atividade.setDescricao("DescriçãoAtividade para teste");
+        this.atividade.setStatus(Status.PENDENTE);
+        this.atividade.setResponsaveis(Arrays.asList(membro));
+
+        this.guilda = new Guilda();
+        this.guilda.setId(1);
+        this.guilda.setNome("NomeGuilda para teste.");
+        this.guilda.setDescricao("Descrição para teste.");
+        this.guilda.setObjetivos("Objetivos para teste.");
+        this.guilda.setLinkDoChat("LinkDoChat para teste.");
+        this.guilda.setMembros(Arrays.asList(membro));
+        this.guilda.setAtividades(Arrays.asList(atividade));
+
+        this.membro.setGuilda(guilda);
+        this.atividade.setGuilda(guilda);
+    }
 }
