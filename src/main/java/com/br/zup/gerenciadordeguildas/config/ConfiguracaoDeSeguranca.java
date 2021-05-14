0ee29344fd3691727f8ff0941f031b171e768ec1
Iris <iris.souza@zup.com.br>
2021-05-14 10:16:23 -0300

    Remover permissões de acesso em endpoints de delete

--- src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/br/zup/gerenciadordeguildas/config/ConfiguracaoDeSeguranca.java
@@ -31,10 +31,6 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC_MATCHERS_POST = {
             "/usuarios/",
             "/login",
-            "/guildas/",
-            "/membros/",
-            "/atividades/",
-            "/atas/"
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {
@@ -44,13 +40,6 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
     };
 
-    private static final String[] PUBLIC_MATCHERS_DELETE = {
-            "/guildas/**",
-            "/membros/**",
-            "/atividades/**",
-            "/atas/**"
-    };
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -58,7 +47,6 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-                .antMatchers(HttpMethod.DELETE, PUBLIC_MATCHERS_DELETE).permitAll()
                 .anyRequest()
                 .authenticated();
 
